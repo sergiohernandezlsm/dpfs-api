f7e1ca8f81507bd5f8cd2326c8d060e6a8a4b2cc
Sergio <sergio.hernandez@littlestarmedia.com>
2020-05-02 13:35:46 +0100

    users seeders done

--- src/database/seeders/20200502121536-demo-users.js
+++ src/database/seeders/20200502121536-demo-users.js
@@ -2,15 +2,18 @@ const faker = require("faker");
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("users", [
-      {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email: faker.internet.email(),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    return queryInterface.bulkInsert(
+      "users",
+      Array.from({ length: 3 }, () => {
+        return {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          email: faker.internet.email(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("users", null, {});
