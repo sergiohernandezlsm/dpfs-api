2bc3cdcaceb0515c13deb3fc207da3b8453ff996
Sergio <36633929+sergiohernandezlsm@users.noreply.github.com>
2020-06-09 10:11:17 +0100

    history, migration, model, seeder (#2)

---
+++ src/database/seeders/20200609085329-demo-histories.js
@@ -0,0 +1,21 @@
+const faker = require("faker");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "histories",
+      Array.from({ length: 4 }, () => {
+        return {
+          title: faker.lorem.words(2),
+          description: faker.lorem.lines(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("histories", null, {});
+  },
+};
