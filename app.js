e79e3020182556ac8d99219feab824659e8f3460
Sergio <sergio.hernandez@littlestarmedia.com>
2020-10-26 09:08:11 +0000

    added cors to api request

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 8080;
 const users = require("./src/routes/users");
 // const db = require("./src/database/models");
 // db.sequelize.sync();
-
+app.use(cors());
 app.use(express.json());
+
 app.use("/", users).get('/', (req, res) => {
   res.send({ message: 'Public endpoint is live' });
 });
